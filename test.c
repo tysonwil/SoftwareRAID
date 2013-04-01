ec30092399faa35c56efd2952baf8a550466a2dc
tedswurf <tedswurf@gmail.com>
2013-04-01 17:30:58 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,28 +1,95 @@
-///////////////////////////////////////////////////////////////////////////////
-// PROGRAM 3 - RAID
-// Title: 
-// Files: 
-// Semester: CS537 Spring 2013
-//
-// PAIR PROGRAMMERS
-//
-// Pair Partner: (Ted) Tianchu Huang thuang33@wisc.edu
-// CS Login: Tianchu
-// Lecturer's Name: Michael Swift
-//
-// Pair Partner: Tyson Williams tjwilliams4@wisc.edu
-// CS Login: twilliam
-// Lecturer's Name: Michael Swift
-//
-// Pair Partner: Adam Thorson adthorson@wisc.edu
-// CS Login: thorson
-// Lecturer's Name: Michael Swift
-//
-//////////////////////////// 80 columns wide //////////////////////////////////
+/*
+test program for the RAID project.
+*/
 
-#include <string.h>
+#include "disk-array.h"
+
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+ 
+int verbose = 1;
+
+int main( int argc, char *argv[] )
+{
+	if(argc!=4) {
+	  printf("use: %s <ndisks> <nblocks> <niter>\n",argv[0]);
+		return 1;
+	}
+
+	int ndisks = atoi(argv[1]);
+	int nblocks = atoi(argv[2]);
+	int niter = atoi(argv[3]);
+
+	disk_array_t da = disk_array_create("myvirtualdisk",ndisks,nblocks);
+	if(!da) {
+		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
+		return 1;
+	}
+
+	//
+	// Randomly read/write to disks
+	//
+	char buffer[BLOCK_SIZE];
+	char cmpbuffer[BLOCK_SIZE];
+	char zerobuffer[BLOCK_SIZE];
+	int i;
+	int failed_disk = -1;
+
+	memset(zerobuffer,0,sizeof(zerobuffer));
+
+#define PROB_FAIL 10
+#define PROB_RECOVER 10
+#define NUM_OPS 2
+#define OP_READ 0
+#define OP_WRITE 1
+	
+	for (i = 0; i < niter; i++) {
+	  if (failed_disk != -1) {
+	    if ((rand() % PROB_RECOVER) == 0) {
+	      disk_array_recover_disk(da,failed_disk);
+	      failed_disk = -1;
+	      continue;
+	    }
+	  } else if ((rand() % PROB_FAIL) == 0) {
+	      failed_disk = rand() % ndisks;
+	      disk_array_fail_disk(da,failed_disk);
+	      continue;
+	  }
+	  int disk = rand() % ndisks;
+	  int block = rand() % nblocks;
+	  switch (rand() % NUM_OPS) {
+	  case OP_READ:
+	    if (disk_array_read(da, disk, block, buffer) == 0) {
+	      memset(cmpbuffer,(disk+1)*(block+1),sizeof(cmpbuffer));
+	      if ((memcmp(cmpbuffer,buffer,sizeof(buffer)) != 0) &&
+		  (memcmp(zerobuffer,buffer,sizeof(buffer)) != 0))
+		printf("Error: mismatch disk %d block %d value %d should be %d or zero\n",
+		       disk,
+		       block,
+		       buffer[0],
+		       cmpbuffer[0]);
+	    }
+	    break;
+	  case OP_WRITE:
+	    memset(buffer,(disk+1)*(block+1),sizeof(buffer));
+	    disk_array_write(da, disk, block, buffer);
+	    printf("Wrote disk %d block %d value %d\n",
+		   disk, block, buffer[0]);
+
+	    break;		       
+	  default:
+	    printf("Illegal operation value\n");
+	    break;
+	  }
+	}
+
+	disk_array_close(da);
+
+	return 0;
+}
+
 
 /*
  * Uses a switch to decide between different RAID systems
@@ -86,108 +153,10 @@ void doRaid4(){
 void doRaid10(){
 
 
+
+
+
 }
-
-
-int main(int argc, char * argv[]){
-
-  char* error_msg = "Error\n";
-	char* opt_list  = "level:strip:disks:size:trace:verbose";
-	int counter     = 0;
-	int level       = -1;
-	int strip       = -1;
-	int disks       = -1;
-	int size        = -1;
-	char* trace     = NULL;
-	int verbose     = 0;
-	int exit_flag   = 0;
-
-	//has appropiate amount of arguments?
-	if((argc != 11) || (argc != 13)){
-		write(STDERR_FILENO, error_msg, strlen(error_msg));
-		exit(-1);
-	}
-
-	//set
-	for(counter = 0; counter < (argc - 1)/2; ++counter){
-		if(strcmp("-level", argv[2*counter+1]) == 0){
-			level = atoi(argv[2*counter+2]);
-
-		}else if(strcmp("-strip", argv[2*counter+1]) == 0){
-			strip = atoi(argv[2*counter+2]);
-
-		}else if(strcmp("-disks", argv[2*counter+1]) == 0){
-			disks = atoi(argv[2*counter+2]);
-
-		}else if(strcmp("-size", argv[2*counter+1]) == 0){
-			size = atoi(argv[2*counter+2]);
-
-		}else if(strcmp("-trace", argv[2*counter+1]) == 0){
-			trace = argv[2*counter+å2];
-		
-		}else if(strcmp("-verbose", argv[2*counter+1]) == 0){
-			verbose = argv[2*counter+2];
-		}
-	}
-
-	//CHECK ARGUMENT VALUES
-	//valid level of RAID?
-	if((level != 0) && (level != 10) && (level != 4) && (level != 5)){
-		flag = 1;
-		//debugging
-		printf("%s\n","Invalid or Not Provided Level");
-	}
-
-	/*******************************
-	COMPLETE
-		CHECK
-			ARGUMENT
-				VALUES
-	*******************************/
-
-	//exit if flag was raised
-	if(flag == 1){
-		write(STDERR_FILENO, error_msg, strlen(error_msg));
-		exit(-1);
-	}
-
-	
-	//create disk array
-	disk_array_t my_disk_array = disk_array_create("MyVirtualDiskArray", disks, size);
-	if(!my_disk_array){
-		//debugging
-		printf("%s\n","Unable to Create Virtual Disks");
-		
-		write(STDERR_FILENO, error_msg, strlen(error_msg));		
-		exit(-1);
-	}
-
-	
-	chooseSystem(level);
-	
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
